b1f1acca8125b1a78015a0afed5a9adf65aefc59
Josh Fisher <jfisherd@gmail.com>
2025-12-08 09:16:06 -0500

    implement StatsDisplayProps in StatsDisplay.tsx

--- src/components/StatsDisplay/StatsDisplay.tsx
+++ src/components/StatsDisplay/StatsDisplay.tsx
@@ -1,11 +1,25 @@
-import type { TextStats } from "../../types";
 import type { StatsDisplayProps } from "../../types";
+
+
+// export interface TextStats {
+//   characterCount: number;
+//   wordCount: number;
+//   readingTime: number; // in minutes
+// }
+ 
+// export interface StatsDisplayProps {
+//   stats: TextStats;
+//   showReadingTime?: boolean;
+// }
+
 
 export const StatsDisplay = (props: StatsDisplayProps) => {
 
     return (
         <>
-            
+            <div>Character count: {props.stats.characterCount}</div>
+            <div>Word count: {props.stats.wordCount}</div>
+            <div>Reading time: {props.stats.readingTime}</div>
         </>
     )
 }
